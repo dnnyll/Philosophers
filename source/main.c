dd42d5e9dd5f0557c84a43da0a7460a36365ac94
dnnyll <leal47danny@gmail.com>
2025-08-26 11:21:11 +0200

    parsing modified

--- source/main.c
+++ source/main.c
@@ -1,13 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daniefe2 <daniefe2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/25 12:12:56 by daniefe2          #+#    #+#             */
+/*   Updated: 2025/08/25 12:15:07 by daniefe2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philosophers.h"
 
 int	main(int argc, char **argv)
 {
 	(void)argv;
-	if (argc < 4 || argc > 5)
+	if (argc < 5 || argc > 6)
 	{
 		printf("incorrect number of arguments.\n");
-		return (-1);
+		return (1);
 	}
-	verify_input_args(argv);
+	if(verify_input_args(argv) == 1);
+		return (1);
+	
 	return (0);
 }
--- source/parser.c
+++ source/parser.c
@@ -1,23 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daniefe2 <daniefe2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/25 12:13:00 by daniefe2          #+#    #+#             */
+/*   Updated: 2025/08/25 12:13:01 by daniefe2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philosophers.h"
 
-
-int	verify_input_args(char **argv)
+int verify_input_args(char **argv)
 {
-	int	input;
+	int	args[5];
 
-	input = ft_atoi(argv[1]);
-	if (input > 200 || input <= 0)
+	args[0] = ft_atoi(argv[1]); // number of philosophers
+	args[1] = ft_atoi(argv[2]); // death time
+	args[2] = ft_atoi(argv[3]); // feasting time
+	args[3] = ft_atoi(argv[4]); // resting time
+	if (argv[5])
+		args[4] = ft_atoi(argv[5]);
+	else
+		args[4] = -1;
+	if (args[0] <= 0 || args[0] > 200)
 		printf("invalid number of philosophers\n");
-	input = ft_atoi(argv[2]);
-	if (input <= 0 || is_valid_digit(argv[2]) == 1)
+	if (args[1] <= 0 || is_valid_digit(argv[2]) == 1)
 		printf("invalid death time\n");
-	input = ft_atoi(argv[3]);
-	if (input <= 0 || is_valid_digit(argv[3]) == 1)
-		printf("invalid feasting time");
-	input = ft_atoi(argv[4]);
-	if (input <= 0 || is_valid_digit(argv[4]) == 1)
-		printf("invalid resting time");
-	if (argv[5] && ((ft_atoi(argv[5])) < 0 || is_valid_digit(argv[5]) == 1))
+	if (args[2] <= 0 || is_valid_digit(argv[3]) == 1)
+		printf("invalid feasting time\n");
+	if (args[3] <= 0 || is_valid_digit(argv[4]) == 1)
+		printf("invalid resting time\n");
+	if (argv[5] && (args[4] < 0 || is_valid_digit(argv[5]) == 1))
 		printf("invalid number of times philosophers must feast\n");
 	return (0);
 }
